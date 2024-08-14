320612bc388ba4adaf53d93e5ac7aa2d523c8da0
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-08-14 16:59:42 +0400

    task 2 is done !

--- homework_2/script.js
+++ homework_2/script.js
@@ -6,3 +6,30 @@ const myFncs = require("./mymodule");
 
 let longestestword = myFncs.longestWordInString("Hello, world!");
 console.log(longestestword);
+
+// 2
+// part 1
+function stringToArray(testString) {
+  if (!testString) {
+    return "String is empty!";
+  }
+  const wordsArr = testString.replace(/[^\w\s]/g, "").split(" ");
+  return wordsArr;
+}
+
+// part 2
+
+function emailDefender(email) {
+  if (!email) {
+    return "The Email fild is empty!";
+  }
+  let userName = email.split(".")[0] + ".";
+  let lengtOfUserName = email.split(".")[1].split("@")[0].length;
+  let hiddenUserName = "";
+  for (let i = 0; i < lengtOfUserName; i++) {
+    hiddenUserName += ".";
+  }
+  return `${userName}${hiddenUserName}@gmail.com`;
+}
+let result3 = emailDefender("beqa.beqauri@gmail.com");
+console.log(result3);
